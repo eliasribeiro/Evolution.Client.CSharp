d5b9c0d574c3935c7357321e5e4e3f055c7bc4d3
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-12-28 11:47:49 -0300

    Remoção de alguns projetos de teste que estavam sem conteúdo.

--- Evolution.Client.CSharp/Evolution.Client.CSharp.UnitTests/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using Evolution.Client.CSharp.Models.Instance;
-global using Evolution.Client.CSharp.Models.Instance.ConnectionStatus;
-global using Evolution.Client.CSharp.Models.Instance.Create;
-global using Evolution.Client.CSharp.Models.Instance.InstanceConnect;
-global using Evolution.Client.CSharp.Models.Instance.LogoutInstance;
-global using Evolution.Client.CSharp.Services.Instances;
-global using Moq;
